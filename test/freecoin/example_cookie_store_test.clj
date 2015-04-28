959724bf6dcf2795674733195555b82e0549680c
Gareth <grogers@thoughtworks.com>
2015-04-28 11:32:32 +0100

    GR - Add example cookie store

---
+++ test/freecoin/example_cookie_store_test.clj
@@ -0,0 +1,14 @@
+(ns freecoin.example-cookie-store-test
+  (:require [midje.sweet :refer :all]
+            [freecoin.example-cookie-store :refer [new-example-store]]))
+
+(def my-super-secret-key "foo")
+
+(fact "Writes data"
+      (let [store (new-example-store my-super-secret-key)]
+        (.write-session store nil {:some-key "some-value"}) => "encrypted-with-foo:{:some-key \"some-value\"}"))
+
+(fact "Reads data"
+      (let [store (new-example-store my-super-secret-key)]
+        (.read-session store "encrypted-with-foo:{:some-key \"some-value\"}") => {:some-key "some-value"}))
+
