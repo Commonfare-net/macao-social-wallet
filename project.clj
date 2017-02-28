e4c6bf646d9149498089f0c7957d44513d851453
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-02-28 14:11:48 +0100

    Got rid of unused reps, updated old reps.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
+(def ns-tracker-version "0.3.1")
+
 (defproject freecoin "0.2.0"
   :description "Freecoin digital currency toolkit"
   :url "http://freecoin.ch"
@@ -6,30 +8,30 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.taoensso/timbre "4.8.0"]
                  [liberator "0.14.1"]
-                 [clj-http "3.1.0"]
+                 [clj-http "3.4.1"]
                  [scenic "0.2.5"]
-                 [ring/ring-core "1.5.0"]
+                 [ring/ring-core "1.5.1"]
                  [ring/ring-anti-forgery "1.0.1"]
-                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-defaults "0.2.3"]
                  [ring.middleware.logger "0.5.0"]
-                 [compojure "1.5.1"]
-                 [org.slf4j/slf4j-simple "1.7.21"]
+                 [compojure "1.5.2"] 
                  [org.clojars.d-cent/stonecutter-oauth "0.2.0-SNAPSHOT"]
-                 [http-kit "2.1.19"]
+                 [http-kit "2.2.0"]
                  [enlive "1.1.6"]
                  [formidable "0.1.10"]
-                 [cheshire "5.6.3"]
+                 [cheshire "5.7.0"]
                  [json-html "0.4.0"]
                  [autoclave "0.1.7" :exclusions [com.google.guava/guava com.google.code.findbugs/jsr305]]
-                 [com.novemberain/monger "3.0.2"]
+                 [com.novemberain/monger "3.1.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [com.tiemens/secretshare "1.4.2"]
-                 [buddy/buddy-hashers "0.14.0"]
+                 [buddy/buddy-hashers "1.2.0"]
                  [simple-time "0.2.1"]
-                 [environ "1.0.3"]
-                 [clojure-humanize "0.2.0"]
+                 [environ "1.1.0"]
+                 [clojure-humanize "0.2.2"]
                  [clj.qrgen "0.4.0"]
                  [clavatar "0.3.0"]
+                 ;; Gossip is a lein tool to generate call-graphs for Clojure code
                  [cc.artifice/lein-gossip "0.2.1"]
                  [circleci/clj-yaml "0.5.5"]]
 
@@ -50,10 +52,9 @@
             "prod" ["with-profile" "production" "run"]
             "test-transactions" ["with-profile" "transaction-graph" "run"]}
   :profiles {:dev [:dev-common :dev-local]
-             :dev-common {:dependencies [[midje "1.8.3"]
-                                         [peridot "0.4.4"]
+             :dev-common {:dependencies [[midje "1.8.3"] 
                                          [kerodon "0.8.0"]
-                                         [ns-tracker "0.3.0"]]
+                                         [ns-tracker ~ns-tracker-version]]
                           :repl-options {:init-ns freecoin.core}
                           :env [[:base-url "http://localhost:8000"]
                                 [:client-id "LOCALFREECOIN"]
@@ -75,7 +76,7 @@
                                   :main transaction-graph.main}]
              :production {:source-paths ["src" "prod"]
                           :main freecoin.main}
-             :uberjar {:dependencies [[ns-tracker "0.3.0"]]
+             :uberjar {:dependencies [[ns-tracker ~ns-tracker-version]]
                        :source-paths ["src" "prod"]
                        :aot :all
                        :main freecoin.main}}
