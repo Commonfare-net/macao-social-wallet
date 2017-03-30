5d2f07fa14bd737b5b5a99235e67d735e75ace80
Jaromil <jaromil@dyne.org>
2017-03-30 16:23:29 +0200

    Revert "web based repl for the admin interface"
    This reverts commit bb283661a1447a542bab355d256dc39bda9cfcbe.
    
    Conflicts:
    	project.clj

--- resources/gorilla-repl-client/js/eventbus.js
+++
@@ -1,9 +0,0 @@
-/*
- * This file is part of gorilla-repl. Copyright (C) 2014-, Jony Hudson.
- *
- * gorilla-repl is licenced to you under the MIT licence. See the file LICENCE.txt for full details.
- */
-
-// The event bus is nothing more than a jquery selection that can be used to register and trigger events.
-
-var eventBus = $({});
