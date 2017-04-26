7da153d7e03f3f0aa720a5afe8f6fea727f1a21b
Jaromil <jaromil@dyne.org>
2017-04-27 00:44:22 +0200

    updates to vagrant for devops (fix #31)

--- ops/freecoin_install.sh
+++ ops/freecoin_install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
+#
 # Copyright (c) 2017 Dyne.org Foundation
 #
 # This file is part of Freecoin
@@ -16,41 +17,57 @@
 # You should have received a copy of the GNU General Public License
 # along with this source code. If not, see <http://www.gnu.org/licenses/>.
 
-repo=https://github.com/d-cent/freecoin
+repo=https://github.com/pienews/freecoin
 
-print "----- entering freecoin provisioning"
+print "== entering freecoin provisioning"
 
+# TODO: check if already created
 useradd -m freecoin
 print "freecoin:freecoin" | chpasswd
 chsh -s /bin/zsh freecoin
 
-print "----- start installing openJDK and mongodb"
-apt-get -qq -y update
-apt-get --yes --force-yes upgrade
+# TODO: check if already installed
+print "== start installing openJDK and mongodb"
+# apt-get -q -y update
+# apt-get --yes --force-yes upgrade
 apt-get --yes --force-yes --no-install-recommends install openjdk-7-jdk mongodb
-apt-get clean
-print "----- end installing openJDK and mongodb"
+# apt-get clean
+print "== end installing openJDK and mongodb"
 
-print "----- start installing leiningen"
-curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein \
-	 -o  /usr/local/bin/lein
-chmod +x /usr/local/bin/lein
-print y | LEIN_ROOT=1 lein upgrade 2> /dev/null
-print "----- end installing leiningen"
-
-print "----- start cloning freecoin"
-pushd /home/freecoin
-touch .zshrc
-sudo -u freecoin git clone $repo freecoin-src
-pushd freecoin-src
-sudo -u freecoin git submodule update --init
-print "----- end cloning freecoin"
+[[ -x /usr/local/bin/lein ]] || {
+	print "== installing leiningen"
+	curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein \
+		 -o  /usr/local/bin/lein
+	chmod +x /usr/local/bin/lein
+	print "== end installing leiningen"
+}
+# print y | LEIN_ROOT=1 lein upgrade 2> /dev/null
 
 
-print "----- start building freecoin"
+[[ -r /home/freecoin/freecoin-src ]] || {
+	print "== start cloning freecoin"
+	pushd /home/freecoin
+	touch .zshrc
+	sudo -u freecoin git clone $repo freecoin-src
+	pushd freecoin-src
+	sudo -u freecoin git submodule update --init
+	print "== end cloning freecoin"
+}
+
+# TODO: perhaps git pull --rebase if updated
+
+print "== start building freecoin"
 sudo -u freecoin lein deps
 sudo -u freecoin lein compile
-print "----- over building freecoin"
+print "== over building freecoin"
 
-# TODO: verify that this command starts freecoin correctly
-sudo -u freecoin lein ring server-headless
+# start freecoin (and also at every boot from rc.local)
+cat <<EOF > /etc/rc.local
+HOME=/home/freecoin &&
+	cd /home/freecoin/freecoin-src &&
+	sudo -b -u freecoin lein ring server-headless
+exit 0
+EOF
+HOME=/home/freecoin &&
+	cd /home/freecoin/freecoin-src &&
+	sudo -b -u freecoin lein ring server-headless
