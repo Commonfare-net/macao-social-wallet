9e7679a7163f44251d03f4125b32363e3adf09c1
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-08-17 16:25:11 +0200

    New freecoin-lib release

--- project.clj
+++ project.clj
@@ -38,7 +38,7 @@
                  [org.clojars.dyne/fxc "0.5.0"]
 
                  ; freecoin core lib
-                 [org.clojars.dyne/freecoin-lib "0.2.0"]
+                 [org.clojars.dyne/freecoin-lib "0.3.0"]
 
                  ; email
                  [com.draines/postal "2.0.2"]]
