7306cfac2013a561a43886af5ee78a88269bcf50
Kieran <kieran.a.gibb@gmail.com>
2018-02-07 12:24:58 +0000

    apt update before install open-jdk to prevent 404 & add email-conf.edn

--- ops/freecoin_install.sh
+++ ops/freecoin_install.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this source code. If not, see <http://www.gnu.org/licenses/>.
 
-repo=https://github.com/pienews/freecoin
+repo=https://github.com/Commonfare-net/macao-social-wallet
 
 print "== entering freecoin provisioning"
 
@@ -28,30 +28,30 @@ chsh -s /bin/zsh freecoin
 
 # TODO: check if already installed
 print "== start installing openJDK and mongodb"
-# apt-get -q -y update
+apt-get -q -y update
 # apt-get --yes --force-yes upgrade
 apt-get --yes --force-yes --no-install-recommends install openjdk-7-jdk mongodb
 # apt-get clean
 print "== end installing openJDK and mongodb"
 
 [[ -x /usr/local/bin/lein ]] || {
-	print "== installing leiningen"
-	curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein \
-		 -o  /usr/local/bin/lein
-	chmod +x /usr/local/bin/lein
-	print "== end installing leiningen"
+  print "== installing leiningen"
+  curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein \
+    -o  /usr/local/bin/lein
+  chmod +x /usr/local/bin/lein
+  print "== end installing leiningen"
 }
 # print y | LEIN_ROOT=1 lein upgrade 2> /dev/null
 
 
 [[ -r /home/freecoin/freecoin-src ]] || {
-	print "== start cloning freecoin"
-	pushd /home/freecoin
-	touch .zshrc
-	sudo -u freecoin git clone $repo freecoin-src
-	pushd freecoin-src
-	sudo -u freecoin git submodule update --init
-	print "== end cloning freecoin"
+  print "== start cloning freecoin"
+  pushd /home/freecoin
+  touch .zshrc
+  sudo -u freecoin git clone $repo freecoin-src
+  pushd freecoin-src
+  sudo -u freecoin git submodule update --init
+  print "== end cloning freecoin"
 }
 
 # TODO: perhaps git pull --rebase if updated
@@ -59,15 +59,19 @@ print "== end installing openJDK and mongodb"
 print "== start building freecoin"
 sudo -u freecoin lein deps
 sudo -u freecoin lein compile
+emailconf=/home/freecoin/freecoin-src/email-conf.edn
+if [ ! -f $emailconf ]; then
+  echo '{:email-server "" :email-user "" :email-pass "" :email-address ""}' > $emailconf
+fi
 print "== over building freecoin"
 
 # start freecoin (and also at every boot from rc.local)
 cat <<EOF > /etc/rc.local
 HOME=/home/freecoin &&
-	cd /home/freecoin/freecoin-src &&
-	sudo -b -u freecoin lein ring server-headless
+  cd /home/freecoin/freecoin-src &&
+  sudo -b -u freecoin lein ring server-headless
 exit 0
 EOF
 HOME=/home/freecoin &&
-	cd /home/freecoin/freecoin-src &&
-	sudo -b -u freecoin lein ring server-headless
+  cd /home/freecoin/freecoin-src &&
+  sudo -b -u freecoin lein ring server-headless
